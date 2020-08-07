1ff7fb9b8550ca34018dee686d8dbdaebb2d4e13
Sulabh Bansal <bansalsulabh13@gmail.com>
2020-08-07 10:38:32 +0530

    adding some more functions

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,42 @@ myweb1(sto) async {
   print(response.body);
 }
 
+myweb2(pul) async {
+  var url = "http://192.168.43.33/cgi-bin/pull.py?x=${pul}";
+  var response = await http.get(url);
+  print(response.body);
+}
+
+myweb3(rmo) async {
+  var url = "http://192.168.43.33/cgi-bin/remove.py?x=${rmo}";
+  var response = await http.get(url);
+  print(response.body);
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   String imname;
   String coname;
   String stop;
+  String pull;
+  String remove;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey,
         appBar: AppBar(
-          title: Text('DOCKER APP'),
+          title: Center(child: Text('DOCKER APP')),
           leading: Icon(Icons.touch_app),
           backgroundColor: Colors.amber,
+          actions: <Widget>[
+            Icon(Icons.dock),
+          ],
         ),
         body: Center(
           child: Container(
-            width: 300,
-            height: 300,
+            width: double.infinity,
+            height: double.infinity,
             color: Colors.amberAccent,
             child: Column(
               children: <Widget>[
@@ -86,6 +103,44 @@ class MyApp extends StatelessWidget {
                     myweb1(stop);
                   },
                   child: Text('Stop'),
+                ),
+                TextField(
+                  onChanged: (value) {
+                    pull = value;
+                  },
+                  autocorrect: false,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter image name",
+                    prefixIcon: Icon(Icons.get_app),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    // print('hi');
+                    myweb2(pull);
+                  },
+                  child: Text('Pull'),
+                ),
+                TextField(
+                  onChanged: (value) {
+                    remove = value;
+                  },
+                  autocorrect: false,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Enter container name",
+                    prefixIcon: Icon(Icons.perm_identity),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    // print('hi');
+                    myweb3(remove);
+                  },
+                  child: Text('Remove'),
                 ),
               ],
             ),
